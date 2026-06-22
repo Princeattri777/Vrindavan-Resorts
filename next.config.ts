98e749e157b56eb9e201720376db46fe57fd9ad9
Prince <149260917+Princeattri777@users.noreply.github.com>
2026-06-22 13:27:48 +0530

    Update basePath and assetPrefix in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
-  basePath: "/Princeattri777",
+  basePath: "/Vrindavan-Resorts",
+  assetPrefix: "/Vrindavan-Resorts/",
 
   images: {
     unoptimized: true,
